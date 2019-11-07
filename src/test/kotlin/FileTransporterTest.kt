83c86a1efbdc1b5b2fd119e5d1b92ef0cf7f0bcc
adam2809 <adam.kule@gmail.com>
2019-11-07 20:07:21 +0000

    Finished skeleton for file transport tests

--- src/test/kotlin/FileTransporterTest.kt
+++ src/test/kotlin/FileTransporterTest.kt
@@ -1,16 +1,46 @@
-import filetransport.FileTransporter
 import filetransport.FileTransporterFactory
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.io.IOException
 
 class FileTransporterTest{
 
     companion object{
         const val DUMMY_FILE_PATH = "/home/adam/code/ytplSynch/src/test/resources/fileTransporterTestDummyFile.txt"
-        const val DUMMY_FILE_DEST_PATH = "/home/adam/code/ytplSynch/src/test/resources/fileTransporterTestDummyFile.txt"
+        const val DUMMY_FILE_DEST_PATH = "/sdcard/ytplSynchTest"
+
+        const val INVALID_PATH = "bibibibiBIBIBIBIBIBI"
     }
+
+    private val transporter = FileTransporterFactory.getInstance()
 
     @Test
     fun transportsFileLinux2Android(){
-        val trasnporter:FileTransporter = FileTransporterFactory.getInstance()
+        transporter.transport(DUMMY_FILE_PATH, DUMMY_FILE_DEST_PATH)
+
+        assertEquals(1,getTestFilesOnDevice().size)
+        assertEquals("fileTransporterTestDummyFile.txt",getTestFilesOnDevice()[0])
+    }
+
+    @Test
+    fun invalidSourcePathThrowsCorrectException(){
+        val e = assertThrows<IOException> {
+            transporter.transport(INVALID_PATH, DUMMY_FILE_DEST_PATH)
+        }
+        assertEquals("Source path ($INVALID_PATH) is invalid",e.message)
+    }
+
+
+    @Test
+    fun invalidDestPathThrowsCorrectException(){
+        val e = assertThrows<IOException> {
+            transporter.transport(DUMMY_FILE_PATH,INVALID_PATH)
+        }
+        assertEquals("Destination path ($INVALID_PATH) is invalid",e.message)
+    }
+
+    fun getTestFilesOnDevice():Array<String>{
+        return emptyArray()
     }
 }
